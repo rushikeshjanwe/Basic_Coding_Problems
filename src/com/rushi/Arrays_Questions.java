85d7507591defdf368fd7e8f4241b7f494116152
rushikeshjanwe <rushikeshjanwe@gmail.com>
2025-07-17 22:16:01 +0530

    New question added

---
+++ src/com/rushi/Arrays_Questions.java
@@ -0,0 +1,24 @@
+package com.rushi;
+
+public class Arrays_Questions {
+
+	
+	public static void main(String[] args) {
+		
+		int arr[]= {1,2,3,4};
+		int x=3;
+		
+		
+		for(int i=0;i<arr.length;i++) {
+			if(arr[i]==x) {
+				System.out.println(arr[i]);
+			}
+		}
+	
+	}
+	
+	
+	
+	
+
+}
--- src/com/rushi/PrimeNo.java
+++ src/com/rushi/PrimeNo.java
@@ -4,31 +4,30 @@ public class PrimeNo {
 	
 	
 
-     static boolean isPrime(int n) {
-    	
-    	 if(n<=1) {
-    		 System.out.println("Both 0 & 1 are not prime numbers");
-    	 }
-    
-    	 for(int i=2;i<=Math.sqrt(n);i++) {
-    		 if(n%i==0) {
-    			 return false;
-    		 }
-    	 }
-    	
-    	 
-    	 return true;
-    }
-
-     
-     public static void main(String[] args) {
-		int limit=15;
+	static boolean isPrime(int n) {
 		
-		for(int i=2;i<=limit;i++) {
+		if(n<=1) {
+			return false;
+		}
+		
+		for(int i=2;i<=Math.sqrt(n);i++) {
+			if(n%i==0) {
+				return false;
+			}
+			
+			
+		}
+		return true;
+	}
+	
+	
+	public static void main(String[] args) {
+		int limit=115;
+		
+		for(int i=0;i<=limit;i++) {
 			if(isPrime(i)) {
 				System.out.print(i+" ");
 			}
-		
 		}
 	}
 }
